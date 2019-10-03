fc2b91a7af47901f2edc8f0cc2e154c99fb0d527
HYUCE <huseyin@marmara.edu.tr>
2019-10-03 11:10:47 +0300

    yeni örnekler

---
+++ kabuk14a.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+clear
+
+dizi_Dosya=()
+
+for dosya in `cut -d',' -f4 ornekveri.csv`
+    do
+    dizi_Dosya=("${dizi_Dosya[@]}" "$dosya")
+
+    done
+
+echo ${dizi_Dosya[*]}
+
+sayi=`cut -d',' -f4 ornekveri.csv| wc -l`
+for yas in {0..3}
+do 
+echo "$yas . veri degeri : ${dizi_Dosya[$yas]} "
+ortalama = ortalama + ${dizi_Dosya[$yas]}
+
+done
+
+echo ortalama / 4 | bc
+
+
+#echo "cevap : ${dizi_Dosya[$deger-1]}"
+
---
+++ kabuk18a.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Birinci $1 ve Ikinci $2 sayilari girdiniz"
+
+echo "Islem olarak $3 yapmak istiyorsunuz"
+
